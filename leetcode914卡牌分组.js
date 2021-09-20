2c7f31c5591346cd0f0061e764474e621acb4dad
liaolonghui <1340881930@qq.com>
2021-09-20 19:29:42 +0800

    看题解

---
+++ leetcode914卡牌分组.js
@@ -0,0 +1,15 @@
+// 。。。。。
+var hasGroupsSizeX = function(deck) {
+  let map = new Map()
+  for(let n of deck){
+      map.set(n,map.has(n)?map.get(n)+1:1)
+  }
+  let arr = [...map.values()]
+  let res = arr[0]
+  return arr.every(i => (res = gcd(res, i)) > 1)
+
+};
+let gcd = (a, b) => (b === 0 ? a : gcd(b, a % b))
+
+
+console.log(hasGroupsSizeX([1,2,3,4,4,3,2,1]))
